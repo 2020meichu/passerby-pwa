0bcac0ea108c2e0c27d789125393ffe747e30613
Leo Kuo <p900372012@gmail.com>
2020-10-24 10:02:30 +0800

    [Sign in][Feature] Add manifest and hint of re-take photo

--- src/components/signUp/IdCardForm.vue
+++ src/components/signUp/IdCardForm.vue
@@ -4,12 +4,16 @@
     h1.fs-34.darkmode-white--text 註冊新帳號
     span.fs-12.darkmode-gray--text 步驟二 上傳身份證
   .pt-6.d-flex.justify-center.align-center
-    .take-photo-section.d-flex.justify-center.align-center.flex-column.mb-7.darkmode-superdark(@click="toggleTakePhoto")
-      div(v-if="!photo")
+    .take-photo-section.d-flex.justify-center.align-center.flex-column.mb-2.darkmode-superdark(@click="toggleTakePhoto")
+      div.d-flex.flex-column.justify-center.align-center(v-if="!photo")
         v-icon.take-photo-section__icon(color='primary', x-large) mdi-camera
-        p.primary--text 點此拍攝身份證
+        p.primary--text.mb-0 點此拍攝身份證
       img(v-else :src="photo")
-  v-btn(color='primary', block, :disabled="isTakePhoto") 上傳
+  div.hint.mb-7(v-if="!photo")
+  div.hint.d-flex.justify-center.align-center.mb-4(@click="toggleTakePhoto" v-else)
+    v-icon.primary--text.mr-1(small) mdi-camera
+    p.primary--text.mb-0 重新拍攝
+  v-btn(color='primary', block, :disabled="!photo") 上傳
   TakePhoto(
     v-if="isTakePhoto"
     @setPhoto="setPhoto"
@@ -53,11 +57,17 @@ export default class extends Vue {
   width: 230px;
   height: 340px;
   border-radius: 12px;
-  > p {
+  > div > p {
     font-size: 12px;
   }
   > img {
     width:100%;
+  }
+}
+.hint {
+  width: 100%;
+  p {
+    font-size: 12px;
   }
 }
 .id-card-input::v-deep > .v-input__prepend-outer {
