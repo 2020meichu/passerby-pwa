48e036c3b0998c8e57d3ac020bd8f33a287a94c0
Leo Kuo <p900372012@gmail.com>
2020-10-24 12:09:23 +0800

    [Sign Up][Feature] Add rule of form inputs

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -4,9 +4,9 @@
   .flex-grow-1
     //- h1.fs-34.darkmode-white--text LOGO
   .indicator.d-flex.justify-center.align-center
-    .indicator__bg.d-flex.justify-center.align-center.mr-2
+    .indicator__bg.d-flex.justify-center.align-center.mr-2(:class="isStep2 ? 'inactive' : 'active'")
       .indicator__bg__dot.darkmode-dark
-    .indicator__bg.d-flex.justify-center.align-center
+    .indicator__bg.d-flex.justify-center.align-center(:class="isStep2 ? 'active' : 'inactive'")
       .indicator__bg__dot.darkmode-dark
   UserForm(v-if='!isStep2')
   IdCardForm(v-else)
@@ -26,7 +26,7 @@ import BackBtn from '@/components/BackBtn.vue'
   }
 })
 export default class SignIn extends Vue {
-  isStep2: boolean = true
+  isStep2: boolean = false
 }
 </script>
 
@@ -38,11 +38,16 @@ export default class SignIn extends Vue {
 .indicator {
   width: 100%;
   height: 34px;
+  .active {
+    background-color: rgba(#263238, 0.3);
+  }
+  .inactive {
+    background-color: rgba(#263238, 0);
+  }
   &__bg {
     width: 16px;
     height: 16px;
     border-radius: 50%;
-    background-color: rgba(#263238, 0.3);
     &__dot {
       width: 8px;
       height: 8px;
