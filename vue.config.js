d216e6e2a29f41b038f58c2a53e8e06e0fa4261a
miayang0513 <a16168972@gmail.com>
2020-11-01 17:03:07 +0800

    [i18n][Feature] Install vue-i18n via "vue add i18n"

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,24 @@
 module.exports = {
   lintOnSave: false,
+
   transpileDependencies: [
     'vuetify'
   ],
+
   configureWebpack: {
     module: {
       rules: [
-        { test: /\.node$/, use: 'node-loader' },
+        { test: /\.node$/, use: 'node-loader' }
       ]
+    }
+  },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: true
     }
   }
 }
