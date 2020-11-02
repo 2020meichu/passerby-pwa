cf48af6a17628a617ebf627b44b186bb94614604
miayang0513 <a16168972@gmail.com>
2020-11-02 09:30:10 +0800

    [i18n][Feature] Slightly implement switching locale language

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -3,62 +3,58 @@
   v-list.list.darkmode-dark
     v-subheader.subheader 使用者相關
     list-item.pl-4.pr-4(
-      v-for="(item, i) in userSettingItems"
-      :key="`user-${i}`"
-      :icon="item.icon"
-      :title="item.title"
-      :subTitle="item.subTitle"
-      :emitFunction="item.emitFunction"
-      @emitLogoutDialog="emitLogoutDialog"
+      v-for='(item, i) in userSettingItems',
+      :key='`user-${i}`',
+      :icon='item.icon',
+      :title='item.title',
+      :subTitle='item.subTitle',
+      :emitFunction='item.emitFunction',
+      @emitLogoutDialog='emitLogoutDialog'
     )
     v-divider
     v-subheader.subheader 應用程式相關
     list-item.pl-4.pr-4(
-      v-for="(item, i) in appSettingItems"
-      :key="`app-${i}`"
-      :icon="item.icon"
-      :title="item.title"
-      :subTitle="item.subTitle"
-      :emitFunction="item.emitFunction"
-      @toggleSheet="toggleSheet"
+      v-for='(item, i) in appSettingItems',
+      :key='`app-${i}`',
+      :icon='item.icon',
+      :title='item.title',
+      :subTitle='item.subTitle',
+      :emitFunction='item.emitFunction',
+      @toggleSheet='toggleSheet'
     )
-    v-bottom-sheet.darkmode-superdark(v-model="sheet")
-      v-sheet.darkmode-superdark(height="169px")
-        v-list.darkmode-superdark
+    v-bottom-sheet.darkmode-superdark(v-model='isLanguageListOpen')
+      v-sheet.rounded-t-20.darkmode-superdark(height='169px')
+        v-list.rounded-t-20.darkmode-superdark
           v-subheader 語言
-          v-list-item.pl-4.pr-4(@click="toggleSheet")
+          v-list-item.pl-4.pr-4(v-for='item in languageList', :key='item.name', @click='switchLocaleLanguage(item.i18n)')
             v-list-item-content
-              v-list-item-title.primary--text 繁體中文
-          v-list-item.pl-4.pr-4(@click="toggleSheet")
-            v-list-item-content
-                v-list-item-title 英文
-    v-overlay(
-      :value="dialog"
-      opacity="0.7"
-    )
-      v-dialog(
-        v-model="dialog"
-        width="279"
-      )
-        v-card.darkmode-dark(width="279")
-          v-card-title 是否登出
-          v-card-actions
-            v-spacer
-            v-btn(color="primary" text @click="dialog = false") 取消
-            v-btn(color="primary" text @click="logout").ml-0 確定
+              v-list-item-title(:class='{ "primary--text": item.i18n === selectedLanguage.i18n }') {{ item.name }}
+    v-dialog(v-model='isLogoutModalOpen', width='279', overlay-opacity='0.7')
+      v-card.rounded-20.darkmode-dark(width='279')
+        v-card-title 是否登出
+        v-card-actions
+          v-spacer
+          v-btn(color='primary', text, @click='isLogoutModalOpen = false') 取消
+          v-btn.ml-0(color='primary', text, @click='logout') 確定
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import { TranslateResult } from 'vue-i18n'
 import ListItem from '@/components/ListItem.vue'
 import { Mutation, Getter } from 'vuex-class'
 
-interface settingItem {
-  type: string;
-  icon: string;
-  title: string;
-  subTitle: string | null;
-  emitFunction: string | null;
+interface SettingItem {
+  type: string
+  icon: string
+  title: string | TranslateResult
+  subTitle: string | null
+  emitFunction: string | null
+}
+
+interface Language {
+  name: string
+  i18n: string
 }
 
 @Component({
@@ -68,72 +64,88 @@ interface settingItem {
 })
 export default class Settings extends Vue {
   @Getter('user/getUsername') public username!: string
-  sheet = false
-  dialog = false
-  settingItems: Array<settingItem> = [
+  isLogoutModalOpen = false
+  isLanguageListOpen = false
+  languageList: Array<Language> = [
     {
-      type: 'user',
-      icon: 'mdi-border-color',
-      title: '更改使用者名稱',
-      subTitle: `目前名稱: ${this.username}`,
-      emitFunction: null
+      name: '繁體中文 (台灣)',
+      i18n: 'tw'
     },
     {
-      type: 'user',
-      icon: 'mdi-lock',
-      title: '更改使用者密碼',
-      subTitle: null,
-      emitFunction: null
-    },
-    {
-      type: 'user',
-      icon: 'mdi-login',
-      title: '帳號登出',
-      subTitle: null,
-      emitFunction: 'emitLogoutDialog'
-    },
-    {
-      type: 'app',
-      icon: 'mdi-web',
-      title: '語言',
-      subTitle: '繁體中文',
-      emitFunction: 'toggleSheet'
-    },
-    {
-      type: 'app',
-      icon: 'mdi-cellphone-cog',
-      title: '應用程式版本',
-      subTitle: '0.0.1',
-      emitFunction: null
+      name: 'English (US)',
+      i18n: 'en'
     }
   ]
+  currentI18n = localStorage.getItem('i18n') || 'en'
 
-  mounted () {
-    this.settingItems[0].subTitle = `目前名稱: ${this.username}`
+  get selectedLanguage(): Language {
+    const index = this.languageList.findIndex((language) => language.i18n === this.currentI18n)
+    return this.languageList[index]
   }
 
-  get userSettingItems (): Array<settingItem> {
-    return this.settingItems.filter(item => {
-      return item.type === 'user'
-    })
+  get userSettingItems(): Array<SettingItem> {
+    return [
+      {
+        type: 'user',
+        icon: 'mdi-border-color',
+        title: this.$t('changeUsername'),
+        subTitle: this.username,
+        emitFunction: null
+      },
+      {
+        type: 'user',
+        icon: 'mdi-lock',
+        title: this.$t('changePassword'),
+        subTitle: null,
+        emitFunction: null
+      },
+      {
+        type: 'user',
+        icon: 'mdi-login',
+        title: this.$t('logout'),
+        subTitle: null,
+        emitFunction: 'emitLogoutDialog'
+      }
+    ]
   }
 
-  get appSettingItems (): Array<settingItem> {
-    return this.settingItems.filter(item => {
-      return item.type === 'app'
-    })
+  get appSettingItems(): Array<SettingItem> {
+    console.log(this.$t('language'))
+    return [
+      {
+        type: 'app',
+        icon: 'mdi-web',
+        title: this.$t('language'),
+        subTitle: this.selectedLanguage.name,
+        emitFunction: 'toggleSheet'
+      },
+      {
+        type: 'app',
+        icon: 'mdi-cellphone-cog',
+        title: this.$t('version'),
+        subTitle: '0.0.1',
+        emitFunction: null
+      }
+    ]
   }
 
-  toggleSheet () {
-    this.sheet = !this.sheet
+  toggleSheet() {
+    this.isLanguageListOpen = !this.isLanguageListOpen
   }
 
-  emitLogoutDialog () {
-    this.dialog = true
+  switchLocaleLanguage(i18n: string): void {
+    this.currentI18n = i18n
+    this.$i18n.locale = i18n
+    localStorage.setItem('i18n', i18n)
+    this.toggleSheet()
   }
 
-  logout () {
-    this.dialog = false
+  emitLogoutDialog() {
+    this.isLogoutModalOpen = true
+  }
+
+  logout() {
+    this.isLogoutModalOpen = false
     localStorage.removeItem('token')
     this.$router.replace('/sign-in')
   }
@@ -150,12 +162,5 @@ export default class Settings extends Vue {
       font-size: 14px;
     }
   }
-}
-.v-sheet {
-  border-top-left-radius: 20px;
-  border-top-right-radius: 20px;
-}
-.v-dialog > .v-card {
-  border-radius: 20px;
 }
 </style>>
